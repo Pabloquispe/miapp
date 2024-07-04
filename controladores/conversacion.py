0d29a0ca849b92ecf539bb4e6e78e1e7247cdbd2
Pabloquispe <131137809+Pabloquispe@users.noreply.github.com>
2024-07-04 16:41:23 -0500

    Update conversacion.py

--- controladores/conversacion.py
+++ controladores/conversacion.py
@@ -9,7 +9,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 from flask import Blueprint, request, jsonify, current_app as app, redirect, url_for
 
 # Configuración de la API de OpenAI
-openai.api_key = os.getenv('OPENAI_API_KEY')
+openai.api_key = os.getenv('API_KEY')
 
 RESERVAS_API_URL = os.getenv('RESERVAS_API_URL')
 # Variable global para almacenar el estado de la conversación
@@ -358,7 +358,7 @@ def handle_message(message):
             'password': conversation_state["password"],
             'estado': 'inicio'
         }
-        response_usuario = requests.post(f'{RESERVAS_API_URL}/usuarios', json=usuario_data)
+        response_usuario = requests.post(f'{API_URL}/usuarios', json=usuario_data)
 
         if response_usuario.status_code == 200:
             conversation_state["usuario_id"] = response_usuario.json()['usuario']
@@ -368,7 +368,7 @@ def handle_message(message):
                 'modelo': conversation_state["modelo"],
                 'año': conversation_state["año"]
             }
-            response_vehiculo = requests.post(f'{RESERVAS_API_URL}/vehiculos', json=vehiculo_data)
+            response_vehiculo = requests.post(f'{API_URL}/vehiculos', json=vehiculo_data)
             if response_vehiculo.status_code == 200:
                 conversation_state["vehiculo_id"] = response_vehiculo.json()['vehiculo']
                 conversation_state["estado"] = "reservar_servicio"
@@ -499,7 +499,7 @@ def handle_message(message):
                 'problema': conversation_state["problema"],
                 'fecha_hora': fecha_hora_reserva.strftime('%Y-%m-%d %H:%M:%S')
             }
-            response = requests.post(f'{RESERVAS_API_URL}/reservas', json=reserva_data)
+            response = requests.post(f'{API_URL}/reservas', json=reserva_data)
 
             if response.status_code == 200:
                 slot.reservado = True
