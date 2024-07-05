1b1605f5686f63e93cae124e09e7370240e1cab8
Pabloquispe <131137809+Pabloquispe@users.noreply.github.com>
2024-07-05 14:37:32 -0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -7,8 +7,5 @@ config_name = os.getenv('FLASK_CONFIG', 'default')
 # Crear la aplicación Flask con la configuración especificada
 app = create_app(config_name)
 
-# Asegurarse de que la aplicación solo se ejecute si este script es ejecutado directamente
 if __name__ == "__main__":
-    # En producción, Gunicorn manejará la ejecución de la aplicación
-    # Pero si estás ejecutando localmente, puedes usar el servidor de desarrollo de Flask
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
