bf08f3a42a908e0017ea1789c6267416d336ed19
Pabloquispe <131137809+Pabloquispe@users.noreply.github.com>
2024-07-07 08:52:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,41 +1,19 @@
-from dotenv import load_dotenv
 import os
 from flask import Flask
-from flask_session import Session
 from flask_migrate import Migrate
 from config import config_by_name
 from modelos.models import db
-from controladores.admin_routes import admin_bp
-from controladores.user_routes import user_bp
-from controladores.auth_routes import auth_bp
-from controladores.main_routes import main_bp
-
-# Cargar las variables de entorno desde el archivo .env
-load_dotenv()
 
 def create_app(config_name):
     """Crea y configura la aplicación Flask."""
-    app = Flask(__name__, template_folder='vistas/templates', static_folder='vistas/static')
+    app = Flask(__name__, template_folder='vistas', static_folder='vistas/static')
     app.config.from_object(config_by_name[config_name])
-
-    # Configurar la sesión
-    app.config['SESSION_TYPE'] = 'filesystem'  # Puedes cambiarlo según tus necesidades
-    Session(app)
-
-    # Verificar si la configuración de la base de datos está correcta
-    if 'SQLALCHEMY_DATABASE_URI' not in app.config:
-        raise RuntimeError("SQLALCHEMY_DATABASE_URI no está configurado")
-
+    
     # Inicializar la base de datos
     db.init_app(app)
     migrate = Migrate(app, db)
 
-    # Registrar Blueprints
-    app.register_blueprint(admin_bp)
-    app.register_blueprint(user_bp)
-    app.register_blueprint(auth_bp)
-    app.register_blueprint(main_bp)
-    
+    # Registrar rutas
     with app.app_context():
         from controladores.routes import register_routes
         register_routes(app)
@@ -44,7 +22,7 @@ def create_app(config_name):
     return app
 
 if __name__ == "__main__":
-    config_name = os.getenv('FLASK_CONFIG') or 'dev'
+    config_name = os.getenv('FLASK_CONFIG') or 'default'
     app = create_app(config_name)
-    app.run(debug=(config_name == 'dev'))
+    app.run(debug=True)
 
