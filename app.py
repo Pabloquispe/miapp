1d998eb2934f2ab2611b28a55bf79aedc7d33e49
Pabloquispe <131137809+Pabloquispe@users.noreply.github.com>
2024-07-07 08:57:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,27 @@
-import os
 from flask import Flask
 from flask_migrate import Migrate
 from config import config_by_name
 from modelos.models import db
-
+from controladores.admin_routes import admin_bp
+from controladores.user_routes import user_bp
+from controladores.auth_routes import auth_bp  # Asegúrate de que este import esté presente
+from controladores.main_routes import main_bp
+import os
 def create_app(config_name):
     """Crea y configura la aplicación Flask."""
-    app = Flask(__name__, template_folder='vistas', static_folder='vistas/static')
+    app = Flask(__name__, template_folder='vistas/templates', static_folder='vistas/static')
     app.config.from_object(config_by_name[config_name])
     
     # Inicializar la base de datos
     db.init_app(app)
     migrate = Migrate(app, db)
 
-    # Registrar rutas
+    # Registrar Blueprints
+    app.register_blueprint(admin_bp)
+    app.register_blueprint(user_bp)
+    app.register_blueprint(auth_bp)  # Asegúrate de que este blueprint esté registrado
+    app.register_blueprint(main_bp)
+    
     with app.app_context():
         from controladores.routes import register_routes
         register_routes(app)
@@ -24,5 +32,4 @@ def create_app(config_name):
 if __name__ == "__main__":
     config_name = os.getenv('FLASK_CONFIG') or 'default'
     app = create_app(config_name)
-    app.run(debug=True)
-
+    app.run(debug=(config_name == 'dev'))
