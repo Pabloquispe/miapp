bbb48c3cbfe2d568d0b8d85433a5d2d3b4551b43
Pabloquispe <131137809+Pabloquispe@users.noreply.github.com>
2024-07-07 09:04:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,31 @@
 from flask import Flask
 from flask_migrate import Migrate
+from flask_session import Session
 from config import config_by_name
 from modelos.models import db
 from controladores.admin_routes import admin_bp
 from controladores.user_routes import user_bp
-from controladores.auth_routes import auth_bp  # Asegúrate de que este import esté presente
+from controladores.auth_routes import auth_bp
 from controladores.main_routes import main_bp
 import os
+
 def create_app(config_name):
     """Crea y configura la aplicación Flask."""
     app = Flask(__name__, template_folder='vistas/templates', static_folder='vistas/static')
     app.config.from_object(config_by_name[config_name])
     
+    # Verificar si la configuración de la base de datos está correcta
+    if 'SQLALCHEMY_DATABASE_URI' not in app.config:
+        raise RuntimeError("SQLALCHEMY_DATABASE_URI no está configurado")
+    
+    # Configuración de la sesión
+    app.config['SESSION_TYPE'] = 'filesystem'
+    app.config['SESSION_PERMANENT'] = False
+    app.config['SESSION_USE_SIGNER'] = True
+    app.config['SESSION_KEY_PREFIX'] = 'flask-session:'
+    app.config['SESSION_COOKIE_NAME'] = 'session'
+    Session(app)
+
     # Inicializar la base de datos
     db.init_app(app)
     migrate = Migrate(app, db)
@@ -19,7 +33,7 @@ def create_app(config_name):
     # Registrar Blueprints
     app.register_blueprint(admin_bp)
     app.register_blueprint(user_bp)
-    app.register_blueprint(auth_bp)  # Asegúrate de que este blueprint esté registrado
+    app.register_blueprint(auth_bp)
     app.register_blueprint(main_bp)
     
     with app.app_context():
